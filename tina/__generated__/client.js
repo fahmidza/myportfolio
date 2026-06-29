512f9387df00b5de22cf82dc3f66cfc6706ccf51
fahmidza <fahmidza@users.noreply.github.com>
2026-06-29 13:44:54 +0700

    chore: generate TinaCMS client and type definition files

--- tina/__generated__/client.js
+++ tina/__generated__/client.js
@@ -1,4 +1,4 @@
 import { createClient } from "tinacms/dist/client";
 import { queries } from "./types.js";
-export const client = createClient({ url: "http://localhost:4001/graphql", token: "dummy-token", queries });
+export const client = createClient({ cacheDir: "d:/10. [Careers]/Github - Portfolio/portfolio/tina/__generated__/.cache/1782715231774", url: "https://content.tinajs.io/2.4/content/93a25f3c-f4cd-4a67-9b94-6e07c8441370/github/main", token: "cbbadf728812868542cfab084b8f3d2b2efa5647", queries });
 export default client;
